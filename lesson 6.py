8257dc6bb35513a33b814276618ebae631b6d62c
VladKonoval <vlad.konoval.17@gmail.com>
2022-05-16 16:20:22 +0300

    lesson 6 16.05

---
+++ lesson 6.py
@@ -0,0 +1,24 @@
+import requests
+def first():
+    pass
+class Human:
+    pass
+rq = requests
+first = first
+nick = Human
+print(requests.__name__)
+print(rq.__name__)
+print(first.__name__)
+print(first.__name__)
+print(nick.__name__)
+print(__name__)
+list = []
+for method in dir(list):
+    print(method)
+print(type(list))
+data = "spring"
+print(hasattr(data,"revers"))
+print(hasattr(data,"index"))
+print(getattr(data,"startwich"))
+print(getattr(data,"startwich",None))
+print(getattr(data,"reverse",None))
