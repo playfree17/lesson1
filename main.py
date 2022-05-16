bd82a1b946350348d3476e5e88b1e5d800aec78f
VladKonoval <vlad.konoval.17@gmail.com>
2022-05-16 15:40:58 +0300

    create file lesson 6.pe for lesson 6

---
+++ main.py
@@ -0,0 +1,11 @@
+class People:
+    print("Russia is a PARASHA")
+    a = 0
+    def __init__(self):
+        self.hp=1000
+        self.speed=25
+    def pri(a):
+        print("↓↓↑↨∟←↔§↑↓→↑↓→←∟↔")
+Sania = People()
+Sania.pri()
+print(Sania.hp, Sania.speed)
---
+++ na dz 5.py
@@ -0,0 +1,13 @@
+class Man1:
+    r = ""
+    def __init__ (self):
+        self.year = input("Years: ")
+
+        r = a
+        print(f"{r} is {self.year} years old")
+class Man2(Man1):
+    def __init__(self):
+        self.r = "Roma"
+        super().__init__()
+Man1()
+Man2()
