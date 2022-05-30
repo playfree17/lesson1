c8ed370c4871444559b43d053164f3597231a322
VladKonoval <vlad.konoval.17@gmail.com>
2022-05-30 16:24:40 +0300

    lesson 10 import requests re.content split

--- lesson 10.py
+++ lesson 10.py
@@ -1,7 +1,34 @@
+
 import requests
-re = requests.get("https://httpbin.org/get")
-print(re.content)
-print(f"Datatype - {type(re.text)}")
+responce = requests.get("https://coinmarketcap.com")
+responce_text = (responce.text)
+responce_parse = responce_text.split("<span>")
+for parse_elem_1 in responce_parse:
+    if parse_elem_1.startswith("$"):
+        print(parse_elem_1)
+
+
+
+
+
+
+
+
+
+# import requests
+# re = requests.post("https://httpbin.org/post",data="Test one",headers={"h1":"Test title"})
+# print(re.text)
+
+
+
+
+
+
+
+# import requests
+# re = requests.get("https://httpbin.org/get")
+# print(re.text)
+# print(f"Datatype - {type(re.text)}")
 
 # import urllib.request
 # opener = urllib.request.build_opener()
