c1a5df30d2a80bd171697ad587b67d145b55d371
VladKonoval <vlad.konoval.17@gmail.com>
2022-05-30 15:47:09 +0300

    lesson 10 import requests

--- lesson 10.py
+++ lesson 10.py
@@ -1,4 +1,13 @@
-import urllib.request
-opener = urllib.request.build_opener()
-res = opener.open("https://httpbin.org/get")
-print(res.read())
+import requests
+re = requests.get("https://httpbin.org/get")
+print(re.content)
+
+
+
+
+
+
+# import urllib.request
+# opener = urllib.request.build_opener()
+# res = opener.open("https://httpbin.org/get")
+# print(res.read())
