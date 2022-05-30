1a9da4d21ab30c6407fefc4e60522d15e99bda33
VladKonoval <vlad.konoval.17@gmail.com>
2022-05-30 15:51:26 +0300

    lesson 10 import requests re.content

--- lesson 10.py
+++ lesson 10.py
@@ -1,11 +1,7 @@
 import requests
 re = requests.get("https://httpbin.org/get")
 print(re.content)
-
-
-
-
-
+print(f"Datatype - {type(re.text)}")
 
 # import urllib.request
 # opener = urllib.request.build_opener()
