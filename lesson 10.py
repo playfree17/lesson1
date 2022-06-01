28f04beed8ac9dddbd1f13eddceff80f9766cea4
VladKonoval <vlad.konoval.17@gmail.com>
2022-06-01 16:13:23 +0300

    cod parser

--- lesson 10.py
+++ lesson 10.py
@@ -1,12 +1,18 @@
-
 import requests
+res_parse_list = []
 responce = requests.get("https://coinmarketcap.com")
 responce_text = (responce.text)
 responce_parse = responce_text.split("<span>")
 for parse_elem_1 in responce_parse:
     if parse_elem_1.startswith("$"):
-        print(parse_elem_1)
-
+        for parse_elem_2 in parse_elem_1.split("</span"):
+            if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
+               res_parse_list.append(parse_elem_2)
+                # print(parse_elem_1)
+bitcoin_ex = res_parse_list[0]
+Ef = res_parse_list[1]
+print("btc - ",bitcoin_ex)
+print("eth - ",Ef)
 
 
 
