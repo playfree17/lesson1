43aa539451c05f872d5c6d11d664728e0a67d4f4
VladKonoval <vlad.konoval.17@gmail.com>
2022-06-01 16:48:26 +0300

    cbs4

---
+++ parse 2.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+response = requests.get("https://coinmarketcap.com")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text,features="html.parser")
+    soup_list = soup.find_all("a",{"href":"/currencies/bitcoin/markets/"})
+    res = soup_list[0].find("span")
+    print(res.text)
