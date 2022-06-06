98f7caa5e2b6892aeca52fe2ec15f4a2749e469a
VladKonoval <vlad.konoval.17@gmail.com>
2022-06-06 14:13:24 +0300

    na dz 11. Parse site, import BeautifulSoup, import requests and import Image.

--- lesson 10.py
+++ lesson 10.py
@@ -7,12 +7,12 @@ for parse_elem_1 in responce_parse:
     if parse_elem_1.startswith("$"):
         for parse_elem_2 in parse_elem_1.split("</span"):
             if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
-               # res_parse_list.append(parse_elem_2)
-                print(parse_elem_1)
-# bitcoin_ex = res_parse_list[0]
-# Ef = res_parse_list[1]
-# print("btc - ",bitcoin_ex)
-# print("eth - ",Ef)
+                res_parse_list.append(parse_elem_2)
+               #  print(parse_elem_1)
+bitcoin_ex = res_parse_list[0]
+Ef = res_parse_list[1]
+print("btc - ",bitcoin_ex)
+print("eth - ",Ef)
 
 
 
